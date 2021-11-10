d58334a9448a833cdbe3c66cc18789449a3a1bf9
carlevison <54800761+carlevison@users.noreply.github.com>
2021-11-10 11:08:27 +0000

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+// 1. Import classes
+//===================
+
+// Import the CloudinaryModule.
+import { CloudinaryModule } from "@cloudinary/angular";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CloudinaryModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
